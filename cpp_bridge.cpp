69c06beda805ef9cde60c687afef6498b1aab688
Milkbag02 <86520716+Milkbag02@users.noreply.github.com>
2023-01-05 04:48:59 -0500

    Update cpp_bridge.cpp

--- cpp_bridge.cpp
+++ cpp_bridge.cpp
@@ -1,1 +1,40 @@
+#include <lua.hpp>
+#include <memory>
+#include <cstdint>
+
+int32_t math_add(int32_t x, int32_t y) {
+  return x + y;
+}
+
+int32_t math_subtract(int32_t x, int32_t y) {
+  return x - y;
+}
+
+int run_cpp_code(lua_State* L) {
+  std::string_view cpp_code{ lua_tostring(L, -1) };
+
+  auto compiled_code = compile(cpp_code);
+
+  execute(compiled_code);
+
+  return 0;
+}
+
+int math_add() 
+
+int main() {
+  lua_State* L = luaL_newstate();
+  
+  luaL_openlibs(L);
+  
+  lua_register(L, "math_add", math_add);
+
+	// Register math_subtract function in global environment
+	lua_register(L, "math_subtract", math_subtract);
+
+  lua_pushcfunction(L, run_cpp_code);
+  lua_setglobal(L, "run_cpp_code");
+  lua_close(L);
+  return 0;
+}
 
